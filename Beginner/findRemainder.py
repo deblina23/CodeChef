dfd7e8629f82be315b55fb457c840fd6148199bb
Deblina Ghosh <deblina.ghosh.kolkata@gmail.com>
2021-02-05 19:49:05 +0530

    Update findRemainder.py
    checkRange code modified

--- Beginner/findRemainder.py
+++ Beginner/findRemainder.py
@@ -6,7 +6,7 @@ def readInputAndLogicUnit():
             for number in range(0,totalTestcase):
                 number1,number2 = map(int,(input().split()))
                 if((checkRange(number1,1,10000)) and (checkRange(number2,1,10000))):
-                    result =(number1%number2)   
+                    result =(number1%number2)
                     resultList.append(result)
         return(resultList)
 
@@ -15,11 +15,11 @@ def printModule(resultSet):
         print(result)
 
 def checkRange(value, minValue,maxValue):
-    if(value in range(minValue,maxValue)):
-            return(True)
+    if((value>=minValue) and (value<=maxValue)):
+        return(True)
     else:
         return(False)
-        
+
 if __name__ == "__main__":
 
     resultSet = readInputAndLogicUnit()
